820ecc370f16ce910f5cb057a3bb478722852a99
shadow12200 <supreethvpatil122008@gmail.com>
2026-06-18 17:40:29 +0530

    got server working on flask

---
+++ backend_python.py
@@ -0,0 +1,7 @@
+from flask import Flask, request, jsonify, send_from_directory
+
+app = Flask(__name__, static_folder="static")
+
+@app.route("/")
+def home():
+    return send_from_directory("static", "index.html")
