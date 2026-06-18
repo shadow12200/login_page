4cb0a3008a9370daa226d3f752fcbfcbcc08a8c1
shadow12200 <supreethvpatil122008@gmail.com>
2026-06-18 22:12:53 +0530

    auth-token generated

--- backend_python.py
+++ backend_python.py
@@ -1,24 +1,43 @@
 from flask import Flask,request,jsonify
+import mysql.connector as m 
 
 app = Flask(__name__, static_folder="static")
-
+passwd='SUDOssy@26'
+db='login_system'
 @app.route("/")
 def home():
     return app.send_static_file("log_in.html")
+
+
+def check_credentials(username, password):
+    con=m.connect(host="localhost", user="root", password=passwd, database=db)
+    cursor=con.cursor()
+    query="SELECT * FROM main_login WHERE username=%s AND password_hash=%s"
+    cursor.execute(query, (username, password))
+    result=cursor.fetchone()
+    cursor.close()
+    con.close()
+    return result
+
+
 
 @app.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
     username = data.get("username")
     password = data.get("password")
-    
-    print(f"Username: {username}")
-    print(f"Password: {password}")
 
-    return jsonify({
-        "success": True,
-        "message": "Data received"
-    })
+    result = check_credentials(username, password)
+    if result:
+        return jsonify({
+            "success": True,
+            "message": "Login successful"
+        })
+    else:
+        return jsonify({
+            "success": False,
+            "message": "Invalid username or password"
+        })
 
 if __name__ == "__main__":
     app.run(debug=True)
